b14056938d595fb12eff12880aa9bfdae70a162a
Ieva <ieva.jaksaityte@gmail.com>
2017-11-14 20:08:44 +0200

    isvedimo antra uzduotis

---
+++ 2-2 uzduotis/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_2_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var metai = 15;
+            Console.WriteLine("Ivestas amzius: " + metai);
+            Console.WriteLine("ivestas amzius {0}", metai);
+        }
+    }
+}
