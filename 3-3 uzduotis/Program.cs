1b4dee361967ba2346621ae1d3f25e829c6e5c2c
Ieva <ieva.jaksaityte@gmail.com>
2017-11-14 21:05:06 +0200

    arit veiksmu trecia uzduotis

---
+++ 3-3 uzduotis/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_3_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 5;
+            var b = 8;
+            var c = 7;
+            Console.WriteLine(a * b * c);
+        }
+    }
+}
