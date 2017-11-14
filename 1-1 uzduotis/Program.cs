ea57ac37387030266826e9a11a9b1595e6d586bd
Ieva <ieva.jaksaityte@gmail.com>
2017-11-14 19:20:31 +0200

    Add project files.

---
+++ 1-1 uzduotis/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_1_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vardas = "ieva";
+            var pavarde = "akfmsdkg";
+            var amzius = 49;
+            var ugis = 1.76f;
+            var svoris = 84; // kg
+            var aukstoji_mokykla = "stulginskis";
+            var kursas = 4;
+            var akademine_grupe = 'a';
+            var studiju_programa = "veterinarine medicina";
+            var kreditai = 160;
+        }
+    }
+}
