ca3d872ef188b8d11691d6d47f5b659f68058bca
Ieva <ieva.jaksaityte@gmail.com>
2017-11-14 21:16:44 +0200

    arit veiksmu 4 ir 5 uzduotys

---
+++ 3-5 uzduotis/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_5_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var skaicius = 5;
+            var daugiklis = 0;
+
+            Console.WriteLine("{0} * {1} = {2}", skaicius, daugiklis, skaicius * daugiklis++);
+            Console.WriteLine("{0} * {1} = {2}", skaicius, daugiklis, skaicius * daugiklis++);
+            Console.WriteLine("{0} * {1} = {2}", skaicius, daugiklis, skaicius * daugiklis++);
+            Console.WriteLine("{0} * {1} = {2}", skaicius, daugiklis, skaicius * daugiklis++);
+        }
+    }
+}
