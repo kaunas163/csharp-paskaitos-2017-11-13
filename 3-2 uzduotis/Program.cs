3894535ed00bf67e92d0d4e12c3342413f3c286c
Ieva <ieva.jaksaityte@gmail.com>
2017-11-14 21:03:29 +0200

    arit veiksmu antra uzduotis

---
+++ 3-2 uzduotis/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_2_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var skaicius = 5;
+            Console.WriteLine(skaicius);
+            Console.WriteLine(skaicius*skaicius);
+            Console.WriteLine(skaicius*skaicius*skaicius);
+
+            Console.WriteLine(Math.Pow(skaicius, 3));
+        }
+    }
+}
