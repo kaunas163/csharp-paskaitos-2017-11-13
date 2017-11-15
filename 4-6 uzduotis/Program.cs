90352f5eb1901fc840bc4052ca63f4931fa681cf
Ieva <ieva.jaksaityte@gmail.com>
2017-11-15 20:06:21 +0200

    ivedimo sesta uzduotis

---
+++ 4-6 uzduotis/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_6_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ivesk trys skaicius");
+            var a = Convert.ToInt32(Console.ReadLine());
+            var b = Convert.ToInt32(Console.ReadLine());
+            var c = Convert.ToInt32(Console.ReadLine());
+
+            var kazkaip_uzsivadinau = (double)(a + b + c) / 3;
+            Console.WriteLine(Math.Round(kazkaip_uzsivadinau, 2));
+        }
+    }
+}
