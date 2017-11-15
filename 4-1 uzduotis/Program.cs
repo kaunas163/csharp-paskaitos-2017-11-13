aba7e4b3a564898acb10e4c85d144f54957f16d1
Ieva <ieva.jaksaityte@gmail.com>
2017-11-15 19:12:36 +0200

    ivedimo pirma uzduotis

---
+++ 4-1 uzduotis/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_1_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("iveskite savo varda:");
+            var vardas = Console.ReadLine();
+            Console.WriteLine("iveskite savo amziu:");
+            var amzius = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("jusu vardas yra {0}, jums yra {1} metai", vardas, amzius);
+        }
+    }
+}
