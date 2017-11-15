f18b76f05a03104890fd9446abf58e5fe73fa50e
Ieva <ieva.jaksaityte@gmail.com>
2017-11-15 21:27:34 +0200

    else if antra uzduotis

---
+++ 6-2 uzduotis/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_2_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("iveskite rezultata");
+            var rez = Convert.ToInt32(Console.ReadLine());
+
+            if (rez == 100)
+            {
+                Console.WriteLine("100");
+            }
+            else if (rez >= 80)
+            {
+                Console.WriteLine("80-100");
+            }
+            else if (rez >= 50)
+            {
+                Console.WriteLine("50-80");
+            }
+            else if (rez >= 20)
+            {
+                Console.WriteLine("20-50");
+            }
+            else if (rez < 20)
+            {
+                Console.WriteLine("< 20");
+            }
+        }
+    }
+}
