6c97c8fea30f91e89ab07101ac19fc7de3070da8
Ieva <ieva.jaksaityte@gmail.com>
2017-11-15 19:45:47 +0200

    ivedimo trecia uzduotis

---
+++ 4-3 uzduotis/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_3_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("iveskite skaiciu");
+            var skaicius = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(skaicius*skaicius);
+        }
+    }
+}
