ec63b7ae12c71180ffa536bbbc77d5043bec7d16
Ieva <ieva.jaksaityte@gmail.com>
2017-11-16 19:04:25 +0200

    uzduotys

---
+++ 7-2 uzduotis/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_2_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("iveskite du skaicius");
+            var a = Convert.ToInt32(Console.ReadLine());
+
+            if (a < 0)
+            {
+                Console.WriteLine(a + " neigiamas");
+            }
+            else if (a > 0)
+            {
+                Console.WriteLine(a + " teigiamas");
+            }
+            else
+            {
+                Console.WriteLine("skaicius yra nulis");
+            }
+        }
+    }
+}
---
+++ 8-1 uzduotis/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_1_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("iveskite tris skaicius:");
+            var a = Convert.ToInt32(Console.ReadLine());
+            var b = Convert.ToInt32(Console.ReadLine());
+            var c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("didziausias skaicius:");
+
+            if (a > b && a > c)
+            {
+                Console.WriteLine(a);
+            }
+            else if (b > c && b > a)
+            {
+                Console.WriteLine(b);
+            }
+            else if (c > a && c > b)
+            {
+                Console.WriteLine(c);
+            }
+            else
+            {
+                Console.WriteLine("skaiciai lygus");
+            }
+        }
+    }
+}
