7219a851f55a4e768afd5ee0324cb8e1ab80b3c5
Ieva <ieva.jaksaityte@gmail.com>
2017-11-17 19:58:42 +0200

    while  ketvirta uzduotis

---
+++ 12-4 uzduotis/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_4_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*var tekstas = "";
+
+            tekstas += "zodis";
+            tekstas += "tekstas";
+
+            Console.WriteLine(tekstas);*/
+
+            var rezultatas = "";
+            var ivedimas = "";
+
+            while (ivedimas != "exit") // kol neivede exit
+            {
+                Console.Write("iveskite zodi arba exit: ");
+                ivedimas = Console.ReadLine();
+
+                if (ivedimas != "exit")
+                {
+                    rezultatas += ivedimas + " ";
+                }
+            }
+            Console.WriteLine(rezultatas);
+        }
+    }
+}
