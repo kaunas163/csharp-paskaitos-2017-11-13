e80b4acd0c059da2aa26cc656f206fd547f2837e
Ieva <ieva.jaksaityte@gmail.com>
2017-11-17 20:08:07 +0200

    while penkta uzduotis

---
+++ 12-5 uzduotis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_5_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var skaicius = 1;
+
+            while (skaicius % 9 != 0)
+            {
+                Console.WriteLine("iveskite skaiciu");
+                skaicius = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("ivedete skaiciu kuris dalinasi is 9!");
+        }
+    }
+}
