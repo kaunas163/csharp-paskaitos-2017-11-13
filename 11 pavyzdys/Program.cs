58269ba1b478317b2fc5f9b935e4fe1a68c554d5
Ieva <ieva.jaksaityte@gmail.com>
2017-11-17 19:05:47 +0200

    begalinio for pavyzdys

---
+++ 11 pavyzdys/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_pavyzdys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // begalinis ciklas - atsargiai!
+            for (;;)
+            {
+                Console.Write("101010110101010");
+            }
+        }
+    }
+}
