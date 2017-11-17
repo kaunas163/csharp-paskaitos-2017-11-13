ba33e8bb5459ae0e424644c9ea7d0fa27ea6e614
Ieva <ieva.jaksaityte@gmail.com>
2017-11-17 20:26:30 +0200

    while sesta uzduotis

---
+++ 12-6 uzduotis/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_6_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+            var slaptas = rnd.Next(1, 11); // [1-10)
+            var spejimas = 0;
+
+            while (spejimas != slaptas)
+            {
+                Console.WriteLine("spekite skaiciu 1-10");
+                spejimas = Convert.ToInt32(Console.ReadLine());
+
+                if (slaptas > spejimas)
+                {
+                    Console.WriteLine("slaptas skaicius didesnis");
+                }
+                else if (slaptas < spejimas)
+                {
+                    Console.WriteLine("slaptas skaicius mazesnis");
+                }
+                else
+                {
+                    Console.WriteLine("jus laimejote!");
+                }
+            }
+        }
+    }
+}
