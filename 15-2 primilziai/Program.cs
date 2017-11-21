7a9a298dfa50e8dba499b550c5a99f0a846c80e7
Ieva <ieva.jaksaityte@gmail.com>
2017-11-21 21:31:47 +0200

    primilziu uzduotis

---
+++ 15-2 primilziai/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_2_primilziai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> primilziai = new List<double>();
+            
+            var programa = new Program();
+            programa.Ivedimas(primilziai);
+            programa.Isvedimas(primilziai);
+
+            Console.WriteLine("Maziausias: " + programa.Maziausias(primilziai));
+            Console.WriteLine("Didziausias: " + programa.Didziausias(primilziai));
+            Console.WriteLine("Vidutinis: " + programa.Vidutinis(primilziai));
+        }
+
+        // įvedimo metodas
+
+        public void Ivedimas(List<double> primilziai)
+        {
+            Console.WriteLine("iveskite primilziu kieki:");
+            var kiek = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < kiek; i++)
+            {
+                Console.Write("iveskite: ");
+                primilziai.Add(Convert.ToDouble(Console.ReadLine()));
+            }
+        }
+
+        // išvedimo metodas
+
+        public void Isvedimas(List<double> primilziai)
+        {
+            foreach (var primilzis in primilziai)
+            {
+                Console.WriteLine(primilzis + " l");
+            }
+        }
+
+        // min, max, average metodai
+
+        public double Maziausias(List<double> primilziai)
+        {
+            return primilziai.Min();
+        }
+
+        public double Didziausias(List<double> primilziai)
+        {
+            return primilziai.Max();
+        }
+
+        public double Vidutinis(List<double> primilziai)
+        {
+            return primilziai.Average();
+        }
+
+        // rasti du didziausius primilzius
+    }
+}
