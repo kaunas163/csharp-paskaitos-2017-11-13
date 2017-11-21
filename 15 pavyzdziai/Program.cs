1ff8319a86451c61139e4ac371ff4f7887142aa1
Ieva <ieva.jaksaityte@gmail.com>
2017-11-21 19:50:41 +0200

    metodu pavyzdziai ir uzduotys

---
+++ 15 pavyzdziai/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_pavyzdziai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var programa = new Program();
+            programa.Pasisveikinti();
+            programa.Pasisveikinti("Ieva");
+            programa.Pasisveikinti("Ieva", "Jaksaityte");
+            programa.Atsisveikinti();
+            programa.Atsisveikinti("Ieva");
+            programa.Atsisveikinti("Ieva", "Jaksaityte");
+            StatinisMetodas();
+
+            var sumosats = programa.Suma(4, 5);
+            Console.WriteLine(sumosats);
+
+            Console.WriteLine(programa.Suma(7, 5));
+            Console.WriteLine(programa.Skirtumas(7, 5));
+            Console.WriteLine(programa.Sandauga(7, 5));
+            Console.WriteLine(programa.Dalmuo(7, 5));
+
+            Console.WriteLine("Iveskite du skaicius:");
+            var pirmas = Convert.ToInt32(Console.ReadLine());
+            var antras = Convert.ToInt32(Console.ReadLine());
+
+            var suma = programa.Suma(pirmas, antras);
+            var skirtumas = programa.Skirtumas(pirmas, antras);
+            var sandauga = programa.Sandauga(pirmas, antras);
+            var dalmuo = programa.Dalmuo(pirmas, antras);
+
+            Console.WriteLine("{0} + {1} = {2}", pirmas, antras, suma);
+            // kitus isvedimus patys
+
+        } // main metodo pabaigos skliaustas
+
+        // rasysim cia
+        /*
+         pasiekiamumas tipas pavadinimas(parametrai)
+         {
+             kodas
+         }
+         */
+
+        public void Pasisveikinti()
+        {
+            Console.WriteLine("Labas krabas");
+        }
+
+        public void Pasisveikinti(string vardas)
+        {
+            Console.WriteLine("Labas, " + vardas);
+        }
+
+        public void Pasisveikinti(string vardas, string pavarde)
+        {
+            Console.WriteLine("Labas, " + vardas + " " + pavarde);
+        }
+
+        public void Atsisveikinti()
+        {
+            Console.WriteLine("Ate ate");
+        }
+
+        public void Atsisveikinti(string vardas)
+        {
+            Console.WriteLine("Ate ate, " + vardas);
+        }
+
+        public void Atsisveikinti(string vardas, string pavarde)
+        {
+            Console.WriteLine("Ate ate, " + vardas + " " + pavarde);
+        }
+
+        public static void StatinisMetodas()
+        {
+            Console.WriteLine("statinis metodas");
+        }
+
+        public int Suma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Skirtumas(int a, int b)
+        {
+            return a - b;
+        }
+
+        // dar apsirasyti daugybos ir dalmens metodus
+
+        public int Sandauga(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Dalmuo(int a, int b)
+        {
+            return (double)a / b;
+        }
+
+    } // class Program skliausto pabaiga
+}
---
+++ 15-1 programa/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_1_programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> atlyginimai = new List<double>();
+        }
+
+        public void Ivedimas(List<double> atlyginimai)
+        {
+            /* 1. paklaust kiek atlyginimų norima suvesti
+             * 2. sukti ciklą nuo 0 iki nurodyto kiekio
+             * 2. 1. cikle išvesti "iveskite i-aji skaiciu"
+             * 2. 2. įvesti ir įkelti į sąrašą skaičių */
+
+            Console.WriteLine("Kiek atlyginimų suvesti?");
+            var kiek = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < kiek; i++)
+            {
+                Console.Write("įveskite {0}-ajį skaičių: ", i+1);
+                atlyginimai.Add(Convert.ToDouble(Console.ReadLine()));
+            }
+        }
+    }
+}
