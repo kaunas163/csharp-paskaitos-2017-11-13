af4c72df19be7d7256068b68e36c0fc3e5c5b890
Ieva <ieva.jaksaityte@gmail.com>
2017-11-24 20:20:03 +0200

    paveldejimas

---
+++ 17 pavyzdziai/Gaisrininkas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_pavyzdziai
+{
+    class Gaisrininkas : Darbuotojas
+    {
+        public int UzgesintiGaisrai { get; private set; }
+
+        public Gaisrininkas(string vardas, string pavarde, int amzius, double alga, double etatas, int uzgesintiGaisrai) : base(vardas, pavarde, amzius, alga, etatas)
+        {
+            UzgesintiGaisrai = uzgesintiGaisrai;
+        }
+
+        public void Isvedimas()
+        {
+            base.Isvedimas();
+            Console.WriteLine("Uzgesinti gaisrai: " + UzgesintiGaisrai);
+            Console.WriteLine();
+        }
+    }
+}
