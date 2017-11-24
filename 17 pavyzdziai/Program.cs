c5772084e0828583b571e43e7006cac9759d4bcb
Ieva <ieva.jaksaityte@gmail.com>
2017-11-24 20:07:15 +0200

    bazines ir paveldincios klasiu objektai ir isvedimas

--- 17 pavyzdziai/Program.cs
+++ 17 pavyzdziai/Program.cs
@@ -10,7 +10,12 @@ namespace _17_pavyzdziai
     {
         static void Main(string[] args)
         {
+            var darbuotojas = new Darbuotojas("ajfb", "jsd", 20, 500, 1);
+            darbuotojas.Isvedimas();
+            Console.WriteLine();
 
+            var policininkas = new Policininkas("afb", "sjdbg", 30, 600, 1, 100, 20);
+            policininkas.Isvedimas();
         }
     }
 }
